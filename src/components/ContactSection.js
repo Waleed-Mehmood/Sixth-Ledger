f92365531caf9aadc4ce14be66062714b969dae4
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2025-01-21 14:58:50 +0500

    website is ready

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import girlImage from "../assets/image 25.png";
 import { MdOutlineShareLocation } from "react-icons/md";
 import { MdOutlineAttachEmail } from "react-icons/md";
@@ -6,43 +6,46 @@ import { IoCall } from "react-icons/io5";
 import axios from "axios";
 
 const ContactSection = () => {
-
   const [formData, setformData] = useState({
-    name:"",
+    name: "",
     email: "",
-    message: ""
-  })
+    message: "",
+  });
   const [responseMessage, setResponseMessage] = useState("");
   const [responseError, setResponseError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleInput = (e)=>{
-    const {name , value} = e.target;
-    setformData((prevData)=>({
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setformData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
-  const handleSubmit =async  (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setLoading(true);
     try {
-      const response = await axios.post("http://localhost:5000/contact", formData);
-      const message = response?.message;
+      const response = await axios.post(
+        "http://localhost:5000/contact",
+        formData
+      );
+      console.log(response);
+      const message = response?.data?.message;
       setResponseMessage(message);
       setformData({
         name: "",
         email: "",
-        message: ""
-      })
+        message: "",
+      });
     } catch (err) {
-      console.log(err)
+      console.log(err);
       setResponseError(err);
-    } 
+    } finally {
+      setLoading(false);
+    }
+  };
 
-   
-  }
-
-  
   console.log("form data", formData);
   return (
     <section className="bg-[#212327] text-white py-12 px-4">
@@ -99,6 +102,7 @@ const ContactSection = () => {
               <input
                 type="text"
                 name="name"
+                value={formData.name}
                 required
                 onChange={handleInput}
                 placeholder="Your Name"
@@ -107,6 +111,7 @@ const ContactSection = () => {
               <input
                 type="email"
                 name="email"
+                value={formData.email}
                 required
                 onChange={handleInput}
                 placeholder="Your Email"
@@ -117,14 +122,20 @@ const ContactSection = () => {
                 rows="5"
                 required
                 name="message"
+                value={formData.message}
                 onChange={handleInput}
                 className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-orange-500 text-black"
               ></textarea>
 
-              {responseMessage && (<p className="text-green-400">{responseMessage}</p>)}
-              {responseError && (<p className="text-red-400">{responseError}</p>)}
-              <button className="w-full bg-orange-500 text-white py-3 rounded-lg hover:bg-orange-600 transition">
-                Contact
+              {responseMessage && (
+                <p className="text-green-400">{responseMessage}</p>
+              )}
+              {responseError && <p className="text-red-400">{responseError}</p>}
+              <button
+                className="w-full bg-orange-500 text-white py-3 rounded-lg hover:bg-orange-600 transition"
+                disabled={loading}
+              >
+                {loading ? "Submitting..." : "Submit"}
               </button>
             </form>
           </div>
